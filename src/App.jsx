ff70d741da66188052d0e18494e181b514a3d0af
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-22 17:49:08 -0400

    load default movie with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import MovieDisplay from "./components/MovieDisplay";
 
@@ -11,40 +11,28 @@ export default function App() {
 
   // Async functions to search for the movie name
   const getMovie = async (searchTerm) => {
-    try {
+    // Search and locate movie by using API key
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`,
+    );
 
-      const safeSearchTerm = encodeURIComponent(searchTerm);
+    // The API sends JSON
+    const data = await response.json();
 
-      const response = await fetch(
-        `https://www.omdbapi.com/?apikey=${apiKey}&t=${safeSearchTerm}`,
-      );
-
-      // Catches server error
-      if (!response.ok) {
-        throw new Error(`Request failed with status ${response.status}`);
-      }
-
-      // The API sends JSON
-      const data = await response.json();
-
-      // Save the movie
-      setMovie(data);
-      // Display movie information
-      console.log("Movie data received:", data);
-    } catch (error) {
-      // Error message
-      console.error("Unable to get the movie:", error);
-    }
+    // Save the movie information
+    setMovie(data);
   };
+
+  // Starting movie information to display
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
 
   return (
     <main>
       <h1>React Movie Search</h1>
 
-      {/* Form receives the function it should run when we submit a title. */}
       <Form moviesearch={getMovie} />
-
-      {/* MovieDisplay will use this movie information in the next step. */}
       <MovieDisplay movie={movie} />
     </main>
   );

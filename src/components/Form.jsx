e7d8a392b74381577e350013d0bd05612ab14864
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-22 14:02:13 -0400

    build basic movie search form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,21 @@
 export default function Form() {
-  return <h2>Movie Search Form</h2>;
+  // Displays the input and the button
+  return (
+    <section>
+      <h2>Search for a Movie</h2>
+
+      <form>
+        <label htmlFor="searchterm">Movie title:</label>
+
+        <input
+          id="searchterm"
+          type="text"
+          name="searchterm"
+          placeholder="Enter a movie title"
+        />
+
+        <input type="submit" value="Search" />
+      </form>
+    </section>
+  );
 }

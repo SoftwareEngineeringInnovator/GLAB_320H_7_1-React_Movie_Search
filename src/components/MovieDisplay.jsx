435f34f67f4a33b97757d00d9e2da4a9a7b645fa
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-22 17:40:21 -0400

    display movie data with conditional rendering

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,4 +1,21 @@
-export default function MovieDisplay() {
-  // Display the movie
-  return <h2>Movie Display</h2>;
+export default function MovieDisplay({ movie }) {
+  // This function displays the movie after the data has loaded
+  const loaded = () => {
+    return (
+      <>
+        <h1>{movie.Title}</h1>
+        <h2>{movie.Genre}</h2>
+        <img src={movie.Poster} alt={movie.Title} />
+        <h2>{movie.Year}</h2>
+      </>
+    );
+  };
+
+  // This function displays a message before a movie has loaded
+  const loading = () => {
+    return <h1>No Movie to Display</h1>;
+  };
+
+  // Ternary operator
+  return movie ? loaded() : loading();
 }
